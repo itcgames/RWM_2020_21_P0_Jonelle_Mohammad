26108bc3707154cac400dff4fa938f07b0030066
JonelleLawler <lawlerjonelle@gmail.com>
2020-10-12 11:25:51 +0100

    Scored update test complete and passed

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -95,5 +95,18 @@ public class TestSuite
         UnityEngine.Assertions.Assert.IsNull(asteroid);
     }
 
+    [UnityTest]
+    public IEnumerator DestroyedAsteroidRaisesScore()
+    {
+        // 1
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = Vector3.zero;
+        GameObject laser = game.GetShip().SpawnLaser();
+        laser.transform.position = Vector3.zero;
+        yield return new WaitForSeconds(0.1f);
+        // 2
+        Assert.AreEqual(game.score, 1);
+    }
+
 }
 
